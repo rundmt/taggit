428ffb1ad356664cf732edefe0836174fdefc064
Derek Tor <derekmuoktor@gmail.com>
2016-02-18 15:26:47 -0800

    all ajax is working properly

--- app/scripts/image.js
+++ app/scripts/image.js
@@ -1,20 +1,71 @@
 chrome.contextMenus.create({"title": "taggit", "id": "main-tag", "contexts":['image']});
+var clientId, clientSecret, scriptLink;
 
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-    chrome.extension.getBackgroundPage().console.log('foo');
+chrome.storage.sync.get('clientId' , function(obj) {
+    clientId = obj.clientId;        
+});
+
+chrome.storage.sync.get('clientSecret' , function(obj) {
+   clientSecret = obj.clientSecret;        
+});
+
+chrome.storage.sync.get('scriptLink' , function(obj) {
+   scriptLink = obj.scriptLink;        
+});
+
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {       
+    //Ajax call to clarifai auth first    
+    // $.ajax({
+    //     method: "POST",
+    //     url: "https://api.clarifai.com/v1/token/",
+    //     data: {
+    //             'grant_type': 'client_credentials',
+    //             'client_id': clientId,
+    //             'client_secret': clientSecret
+    //           }
+    // })
+    // .done(function( msg ) { 
+    //      var accessToken = msg['access_token'];         
+    //      $.ajax({
+    //         method: "POST",
+    //         url: "https://api.clarifai.com/v1/tag/",
+    //         data: { url: info.srcUrl},
+    //         beforeSend: function(xhr, settings) { xhr.setRequestHeader('Authorization','Bearer ' + accessToken); }
+    //     })
+    //     .done(function( response ) {
+    //         var tagData = response['results'][0]['result']['tag']['classes'].join(", ");                  
+    //         alert(tagData);
+    //     });
+    // });
     
-    //Ajax call to clarifai auth first
-    
-    //Ajax call to sheet. Cross-Fingers
     
     
+    //Ajax call to sheet. Cross-Fingers  
     $.ajax({
         method: "POST",
-        url: "https://script.google.com/macros/s/AKfycbyjVQF99qwWwiD5t5fpRZTHIwY9a4zNfsRoCp-10ytUUGcoHD4/exec",
-        data: { url: info.srcUrl}
+        url: scriptLink,
+        data: { 
+                url: info.srcUrl,
+                docId: '1DjSqLjyQhy5kvbfTS0kkbWpYfwVojL2aX1GipgLJNM8',
+                tags: 'tree, green, forrest, stuff'
+              }
     })
     .done(function( msg ) {
         
         alert( msg );   
     });
 });
+
+function tagPhoto(msg) {
+        var accessToken = msg['access_token'];          
+         $.ajax({
+            method: "POST",
+            url: "https://api.clarifai.com/v1/tag/",
+            data: { url: info.srcUrl},
+            beforeSend: function(xhr, settings) { xhr.setRequestHeader('Authorization','Bearer ' + accessToken); }
+        })
+        .done(function( msg ) {      
+            alert( msg );   
+        });
+    }
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,37 +1,21 @@
 'use strict';
 $(document).ready(function(){
-    console.log("ready");
+   var clientId = $('#clientId').val();
+   var clientSecret = $('#clientSecret').val();
+   var scriptLink = $('#scriptLink').val();
+    
    $('#settings-id').submit(function(e){
        e.preventDefault();
        var clientId = $('#clientId').val();
        var clientSecret = $('#clientSecret').val();
        var scriptLink = $('#scriptLink').val();
-       
-       $.ajax({
-            method: "POST",
-            url: "https://api.clarifai.com/v1/token/",
-            data: {
-                grant_type: 'client_credentials',
-                client_id: clientId,
-                client_secret: clientSecret,
-            }
-        })
-        .done(function( msg ) {            
-            chrome.storage.sync.set({
-                clientId: clientId,
-                clientSecret: clientSecret,       
-                accessToken: msg['access_token'],         
-                scriptLink: scriptLink
-            }, function() {
-            // Update status to let user know options were saved.
-                alert('saved');
-            });                        
-        });
-       
+
+       chrome.storage.sync.set({
+            clientId: clientId,
+            clientSecret: clientSecret,           
+            scriptLink: scriptLink
+       }, function() {
+           $('#alert-node').html('Preferences Have Been Saved');            
+       });
    });
-        // chrome.storage.sync.get('clientId' , function(x) {
-        // // Update status to let user know options were saved.
-        //     alert(JSON.stringify(x));
-        // });
-   
 })
