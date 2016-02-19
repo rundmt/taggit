909e084c2e402faaf612f686dbd380292b5e7d62
Derek Tor <derekmuoktor@gmail.com>
2016-02-18 17:42:39 -0800

    refactored to use promises

--- app/scripts/image.js
+++ app/scripts/image.js
@@ -1,71 +1,59 @@
-chrome.contextMenus.create({"title": "taggit", "id": "main-tag", "contexts":['image']});
 var clientId, clientSecret, scriptLink;
 
+chrome.contextMenus.create({"title": "taggit", "id": "main-tag", "contexts":['image']});
+
 chrome.storage.sync.get('clientId' , function(obj) {
-    clientId = obj.clientId;        
+    clientId = obj.clientId;
 });
 
 chrome.storage.sync.get('clientSecret' , function(obj) {
-   clientSecret = obj.clientSecret;        
+   clientSecret = obj.clientSecret;
 });
 
 chrome.storage.sync.get('scriptLink' , function(obj) {
-   scriptLink = obj.scriptLink;        
+   scriptLink = obj.scriptLink;
 });
 
 
-chrome.contextMenus.onClicked.addListener(function(info, tab) {       
-    //Ajax call to clarifai auth first    
-    // $.ajax({
-    //     method: "POST",
-    //     url: "https://api.clarifai.com/v1/token/",
-    //     data: {
-    //             'grant_type': 'client_credentials',
-    //             'client_id': clientId,
-    //             'client_secret': clientSecret
-    //           }
-    // })
-    // .done(function( msg ) { 
-    //      var accessToken = msg['access_token'];         
-    //      $.ajax({
-    //         method: "POST",
-    //         url: "https://api.clarifai.com/v1/tag/",
-    //         data: { url: info.srcUrl},
-    //         beforeSend: function(xhr, settings) { xhr.setRequestHeader('Authorization','Bearer ' + accessToken); }
-    //     })
-    //     .done(function( response ) {
-    //         var tagData = response['results'][0]['result']['tag']['classes'].join(", ");                  
-    //         alert(tagData);
-    //     });
-    // });
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
     
-    
-    
-    //Ajax call to sheet. Cross-Fingers  
-    $.ajax({
-        method: "POST",
-        url: scriptLink,
-        data: { 
-                url: info.srcUrl,
-                docId: '1DjSqLjyQhy5kvbfTS0kkbWpYfwVojL2aX1GipgLJNM8',
-                tags: 'tree, green, forrest, stuff'
-              }
-    })
-    .done(function( msg ) {
-        
-        alert( msg );   
-    });
-});
+    clarifaiAuth(clientId, clientSecret)
+    .then(tagPhoto)
+    .then(sendToSheet);
 
-function tagPhoto(msg) {
-        var accessToken = msg['access_token'];          
-         $.ajax({
+    function clarifaiAuth(id, secret){
+        return $.ajax({
+            method: "POST",
+            url: "https://api.clarifai.com/v1/token/",
+            data: {
+                'grant_type': 'client_credentials',
+                'client_id': id,
+                'client_secret': secret
+            }
+        });
+    }
+
+    function tagPhoto(res, textStatus, jqXHR) {
+        var accessToken = res['access_token'];
+        return $.ajax({
             method: "POST",
             url: "https://api.clarifai.com/v1/tag/",
             data: { url: info.srcUrl},
             beforeSend: function(xhr, settings) { xhr.setRequestHeader('Authorization','Bearer ' + accessToken); }
-        })
-        .done(function( msg ) {      
-            alert( msg );   
         });
     }
+
+    function sendToSheet(res, textStatus, jqXHR){
+        var tags = res['results'][0]['result']['tag']['classes'].join(", ")
+        
+        return $.ajax({
+            method: "POST",
+            url: 'https://script.google.com/macros/s/AKfycbyjVQF99qwWwiD5t5fpRZTHIwY9a4zNfsRoCp-10ytUUGcoHD4/exec',
+            data: {
+                url: info.srcUrl,
+                docId: scriptLink,
+                tags: tags
+            }
+        });
+    }
+});
--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,9 +1,23 @@
 'use strict';
-$(document).ready(function(){
-   var clientId = $('#clientId').val();
-   var clientSecret = $('#clientSecret').val();
-   var scriptLink = $('#scriptLink').val();
-    
+var clientId, clientSecret, scriptLink;
+
+chrome.storage.sync.get('clientId' , function(obj) {    
+    clientId = obj.clientId;
+    $('#clientId').val(clientId);
+});
+
+chrome.storage.sync.get('clientSecret' , function(obj) {
+    clientSecret = obj.clientSecret;       
+    $('#clientSecret').val(clientSecret); 
+});
+
+chrome.storage.sync.get('scriptLink' , function(obj) {
+    scriptLink = obj.scriptLink;
+    $('#scriptLink').val(scriptLink);    
+});
+
+
+$(document).ready(function(){   
    $('#settings-id').submit(function(e){
        e.preventDefault();
        var clientId = $('#clientId').val();
